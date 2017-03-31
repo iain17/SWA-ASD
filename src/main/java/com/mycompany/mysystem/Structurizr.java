4b63e4be5d9ce2e6dfaa0820ee51b76904b4cb96
Iain Munro <iain17@me.com>
2017-03-31 23:49:22 +0200

    changes made today

--- src/main/java/com/mycompany/mysystem/Structurizr.java
+++ src/main/java/com/mycompany/mysystem/Structurizr.java
@@ -32,43 +32,51 @@ public class Structurizr {
 
         //Build general software system
         SoftwareSystem softwareSystem = model.addSoftwareSystem("Software System", "A real-time ride sharing service");
-        driver.uses(softwareSystem, "Registers trips");
-        passenger.uses(softwareSystem, "Finds travel plans and registers trip");
-        admin.uses(softwareSystem, "Monitors the system.");
+        driver.uses(softwareSystem, "Registers trips using");
+        passenger.uses(softwareSystem, "Finds travel plans and registers trip using");
 
         //Add external software systems this software system uses.
         SoftwareSystem publicTransportApi = model.addSoftwareSystem("Public transport API", "Returns buss and train information.");
         softwareSystem.uses(publicTransportApi, "Fetches public transportation from");
 
-        SoftwareSystem sas = model.addSoftwareSystem("SAS Rooster", "Contains the calendar of a student/teacher");
+        SoftwareSystem sas = model.addSoftwareSystem("SAS schedule", "Contains the calendar of a student/teacher");
         softwareSystem.uses(sas, "Fetches the appointments/classes using a webcal feed from");
 
+        SoftwareSystem datadog = model.addSoftwareSystem("Datadog", "Contains metrics and logging");
+        softwareSystem.uses(datadog, "Sends metrics and logs to");
+        admin.uses(datadog, "Monitors the system through");
+
         //Build mobile app.
-        Container mobileApplication = softwareSystem.addContainer("Smart Mobility mobile APP", "The Smart Mobility mobile APP allows users to interface the Smart Mobility API on their mobile phones.", "Android React app");
+        Container mobileApplication = softwareSystem.addContainer("Smart Mobility mobile APP", "Mobile APP to interface the API.", "Android React (web/mob)app");
         driver.uses(mobileApplication, "Registers trips");
         passenger.uses(mobileApplication, "Finds travel plans and registers trip");
-        admin.uses(mobileApplication, "Monitors the system.");
         mobileApplication.uses(sas, "Fetches the appointments/classes using a webcal feed from");
 
         //Build gateway
-        Container gateway = softwareSystem.addContainer("Smart Mobility API gateway", "The Smart Mobility mobile APP allows users to register trips, plan/register for travel plans and monitor the total system.", "Spring MVC on Apache Tomcat");
+        Container gateway = softwareSystem.addContainer("Smart Mobility gateway", "API gateway aggregating results invoking multiple microservices", "Spring MVC on Apache Tomcat");
         gateway.addTags(MICROSERVICE_TAG);
+
         mobileApplication.uses(gateway, "Uses", "HTTPS");
 
-        //travelPlan service
-        Container travelPlanService = softwareSystem.addContainer("Travel plan Service", "The point of access for information.", "Java and Spring Boot");
-        travelPlanService.addTags(MICROSERVICE_TAG);
-        travelPlanService.uses(publicTransportApi, "Fetches public transportation from", "JSON/HTTPS");
-        gateway.uses(travelPlanService, "Fetches travel plans information using", "JSON/HTTPS", InteractionStyle.Synchronous);
+        //Auth service
+        Container authService = softwareSystem.addContainer("Auth Service", "The point of auth info.", "Java and Spring Boot");
+        authService.addTags(MICROSERVICE_TAG);
+        gateway.uses(authService, "Validates auth tokens", "JSON/HTTPS", InteractionStyle.Synchronous);
+
+        //Trip service
+        Container tripService = softwareSystem.addContainer("Trip service", "The point of access for trip and travel plans information.", "Java and Spring Boot");
+        tripService.addTags(MICROSERVICE_TAG);
+        gateway.uses(tripService, "Fetches and creates trips using", "JSON/HTTPS", InteractionStyle.Synchronous);
 
         //Registered trips database
         Container customerDatabase = softwareSystem.addContainer("Trip Database", "Stores registered trips.", "MySQL");
         customerDatabase.addTags(DATABASE_TAG);
-        travelPlanService.uses(customerDatabase, "Reads to and writes from", "JDBC", InteractionStyle.Synchronous);
+        tripService.uses(customerDatabase, "Reads to and writes from", "JDBC", InteractionStyle.Synchronous);
 
         //User service
         Container userService = softwareSystem.addContainer("User Service", "The point of access for user information.", "Java and Spring Boot");
         userService.addTags(MICROSERVICE_TAG);
+        userService.uses(authService, "Fetches new auth token from");
         gateway.uses(userService, "Fetches user information using", "JSON/HTTPS", InteractionStyle.Synchronous);
 
         //User database
@@ -76,16 +84,22 @@ public class Structurizr {
         userDatabase.addTags(DATABASE_TAG);
         userService.uses(userDatabase, "Reads to and writes from", "JDBC", InteractionStyle.Synchronous);
 
+        //Travel service
+        Container travelService = softwareSystem.addContainer("Travel Service", "The point of access for travel plan information.", "Java and Spring Boot");
+        travelService.addTags(MICROSERVICE_TAG);
+        travelService.uses(tripService, "fetches registered trips from");
+        travelService.uses(publicTransportApi, "Fetches public transportation from", "JSON/HTTPS");
+        gateway.uses(travelService, "Fetches travel plans and registers for them from", "JSON/HTTPS", InteractionStyle.Synchronous);
+
         //Describe components in mobile app.
         Component homeController = mobileApplication.addComponent("Some Controller", "A description of some controller.", "iOS UIViewController");
 
-        //Describe components in gateway.
-//        Component someController = gateway.addComponent("Some Controller", "A description of some controller.", "Spring MVC RestController");
-//        Component someService = gateway.addComponent("Some Service", "A description of some service.", "Spring Bean");
-//        Component someRepository = gateway.addComponent("Some Repository", "A description of some repository.", "Spring Data");
-//        mobileApplication.uses(someController, "Uses", "JSON/HTTPS");
-//        someController.uses(someService, "Uses");
-//        someService.uses(someRepository, "Uses");
+        //Describe components in user service.
+        Component userServiceComponent = userService.addComponent("User component", "A crud service, allowing changes to a user", "Spring Service");
+        Component userRepositoryComponent = userService.addComponent("User repository", "Repository class for User domain objects All method names are compliant with Spring Data naming.", "Spring Repository");
+        mobileApplication.uses(userServiceComponent, "Uses", "JSON/HTTPS");
+        userServiceComponent.uses(userRepositoryComponent, "Uses");
+        userRepositoryComponent.uses(userDatabase, "Reads to and writes from", "JDBC", InteractionStyle.Synchronous);
 
         //
         // Create the views
@@ -103,17 +117,21 @@ public class Structurizr {
         containerView.addAllPeople();
         containerView.addAllContainers();
         containerView.addAllElements();
-        containerView.setPaperSize(PaperSize.A5_Landscape);
+//        Relationship relationship = new Relationship(containerView, datadog, "Sends metrics/logs to", "HTTP/JSON", InteractionStyle.Asynchronous);
+//        RelationshipView relationshipView = new RelationshipView(relationship);
+        containerView.setPaperSize(PaperSize.Slide_16_9);
 
 
         ComponentView componentViewFrontMobileApp = viewSet.createComponentView(mobileApplication, "Components mobile application", "Components diagram for the frontend of the Smart Mobility project.");
         componentViewFrontMobileApp.addAllComponents();
         componentViewFrontMobileApp.setPaperSize(PaperSize.A5_Landscape);
 
-//        ComponentView componentViewBackend = viewSet.createComponentView(gateway, "Components backend", "Components diagram for the backend of the Smart Mobility project.");
-//        componentViewBackend.addAllContainers();
-//        componentViewBackend.addAllComponents();
-//        componentViewBackend.setPaperSize(PaperSize.A5_Landscape);
+        //Containers for user service.
+        ComponentView userServiceComponentView = viewSet.createComponentView(userService, "Components user service", "Components diagram for the user service.");
+        userServiceComponentView.add(userServiceComponent);
+        userServiceComponentView.add(userRepositoryComponent);
+        userServiceComponentView.add(userDatabase);
+        userServiceComponentView.setPaperSize(PaperSize.A5_Landscape);
 
         // tag and style some elements
         Styles styles = viewSet.getConfiguration().getStyles();
